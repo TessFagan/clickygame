f151787a9b9b6e63277d3297cf3e7f0729396ba8
Tess Fagan <TessFagan@Tesss-MacBook-Pro.local>
2019-08-07 19:11:56 -0400

    pass state through component as attribute?

--- client/src/App.js
+++ client/src/App.js
@@ -11,26 +11,16 @@ class App extends Component {
   state = {
     currentscore: 0,
     highscore: 0,
-    picturestatus: [
-      { id: 1, clicked: friends[0].clicked },
-      { id: 2, clicked: friends[1].clicked },
-      { id: 3, clicked: friends[2].clicked },
-      { id: 4, clicked: friends[3].clicked },
-      { id: 5, clicked: friends[4].clicked },
-      { id: 6, clicked: friends[5].clicked },
-      { id: 7, clicked: friends[6].clicked },
-      { id: 8, clicked: friends[7].clicked },
-      { id: 9, clicked: friends[8].clicked },
-      { id: 10, clicked: friends[9].clicked },
-      { id: 11, clicked: friends[10].clicked },
-      { id: 12, clicked: friends[11].clicked }
-    ]
+    clicked: " "
   };
 
   handleClick = () => {
-    this.setState({ currentscore: this.state.currentscore + 1 });
-    console.log("clicked")
-    console.log(this.state.picturestatus[0])
+    this.setState({
+      currentscore: this.state.currentscore + 1,
+      highscore: this.state.currentscore + 1,
+      clicked: true
+    });
+    console.log(this)
   };
 
   render() {
@@ -52,7 +42,7 @@ class App extends Component {
         </Navbar>
         <Instructions />
         <Wrapper>
-          <Cardgroup handleClick={this.handleClick} />
+          <Cardgroup handleClick={this.handleClick} state={this.state} />
         </Wrapper>
       </div >
     );
--- client/src/components/cardgroup.js
+++ client/src/components/cardgroup.js
@@ -6,19 +6,16 @@ function Cardgroup(props) {
 
 
   const pictureCards = friends.map(friend =>
-    <div className="card" onClick={props.handleClick}>
+    <div className="card" onClick={props.handleClick} state={props.state} key={friend.id}>
       <div className="img-container">
         <img
-          key={friend.id}
           alt={friend.name}
           src={friend.image}
           id={friend.id}
+          clicked={friend.clicked}
         />
       </div>
     </div>);
-
-  friends.map(function (friend) { console.log(friend) });
-
 
 
   console.log(pictureCards)
